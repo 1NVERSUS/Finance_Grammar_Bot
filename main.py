843bac4884e79f11ed1664dd415f4e9996f6a996
1NVERSUS <131791308+1NVERSUS@users.noreply.github.com>
2023-06-20 16:30:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -16,4 +16,26 @@ def income_handler(message):
     bot.send_message(message.chat.id, 'Напиши сумму, которую ты заработал')
     bot.register_next_step_handler(message, income)
 def income(message):
-    print(message.json()['text'])
+    f = open('income.txt', 'r+')
+    for line in f:
+        x = str(int(line) + int(message.text))
+        f.seek(0)
+        f.truncate()
+        f.write(x)
+    f.close()
+@bot.message_handler(commands=['consumption'])
+def consumption(message):
+    bot.send_message(message.chat.id, 'Напиши сумму, которую ты потратил')
+    bot.register_next_step_handler(message, consumption)
+
+def consumption(message):
+    d = open('consumption.txt', 'r+')
+    for line in d:
+        x = str(int(line) + int(message.text))
+        d.seek(0)
+        d.truncate()
+        d.write(x)
+    d.close()
+
+
+bot.infinity_polling()
