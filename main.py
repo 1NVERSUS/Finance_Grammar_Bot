2e786bd55a01949bee99ee808afbe446b6d87044
1NVERSUS <131791308+1NVERSUS@users.noreply.github.com>
2023-06-20 14:04:29 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+import telebot
+from telebot import types
+token='6108360249:AAHC6BHyPpZtcEHLA7Lf4b3v3-WX8cqeP7g'
+bot = telebot.TeleBot(token)
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id,'Привет! Я бот-помощник по финансовой граматности')
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    bt1 = types.KeyboardButton('income')
+    markup.add(bt1)
+
+@bot.message_handler(commands=['income'])
+def income_handler(message):
+    f = open('income.txt', 'w')
+    f.write(message)306
+
+
+bot.infinity_polling()
