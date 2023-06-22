4a125462260ed94c08d3399a0245f3ea4326f5b2
oraboni130 <137176206+oraboni130@users.noreply.github.com>
2023-06-22 13:05:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -30,13 +30,6 @@ def start(message):
         consum = 0
         m[idt] = incom
         n[idt] = consum
-    bot.send_message(message.chat.id, f'''Добрый день.
-На данный момент доходы - {m[idt]},
-а расходы - {n[idt]}
-Ваша прибыль - {m[idt] - n[idt]}''')
-    if m[idt]-n[idt] < 0:
-        bot.send_message(message.chat.id, 'Мы советуем вам меньше тратить денег и не писать мне')
-
 @bot.message_handler(commands=['income'])
 def income_handler(message):
     bot.send_message(message.chat.id, 'Напиши сумму, которую ты заработал')
@@ -90,7 +83,7 @@ def dream3(message):
     if idt not in k:
         k[idt] = [sum, tme]
     print(k)
-    bot.send_message(message.chat.id, f'Вам нужно оплачивать {k[idt][0]/k[idt][1]}')
+    bot.send_message(message.chat.id, f'Вам нужно оплачивать по {k[idt][0]/k[idt][1]} в месяц')
 
 @bot.message_handler(commands=['reset'])
 def reset(message):
@@ -101,12 +94,24 @@ def reset(message):
 @bot.message_handler(commands=['show'])
 def show(message):
     idt = message.from_user.id
-    bot.send_message(message.chat.id, f'''Ваши доходы: {m[idt]}
-Ваши расходы: {n[idt]}''')
-
-
-
-
+    bot.send_message(message.chat.id, f'''Ваши доходы: {m[idt]} ({m[idt]/m[idt]+n[idt]}%)
+Ваши расходы: {n[idt]} ({n[idt]/m[idt]+n[idt]}%)
+Ваша прибыль/убытки: {m[idt] - n[idt]}''')
+    if ((m[idt] - n[idt])/m[idt])*100 <= 3:
+        ________('''Я предлагаю вам несколько вариантов увеличения доходов:
+        1.Мотивировать повышение зарплаты на текущем месте работы.
+        2.Сменить работу на более высокооплачиваемую.
+        3.Найти подработку, в том числе в качестве фрилансера.
+        Также есть несколько вариантов уменьшения расходов:
+        1.Экономия на жилищно-комунальных услугах.
+        2.Экономия на мобильной связи. Сегодня мобильные операторы и интернет провайдеры предлагают выгодные тарифы - просто ознакомьтесь и выберите подходящий!
+        3.Экономия на одежде и обуви. Даже дизайнерские вещи можно покупать дешево на распродажах.
+        4.Экономия на досуге и развлечениях. Здесь все должно быть в пределах нормы: если отказ от хобби резко снижает качество вашей жизни, то экономия на нем может вызвать стресс. Этот вид расходов оптимизируйте обдуманно.''')
+    if ((m[idt] - n[idt])/m[idt])*100 > 3 and ((m[idt] - n[idt])/m[idt])*100 <= 20:
+        ________('''Ваши доходы превышают рассходы, так держать! Если вы желаете ещё больший доход, я советую вам преобрести акции или другие ценные бумаги. Для начала следует получить базовые знания об инвестициях из интернет-источников.
+        Инвестиции это риск,поэтому вам решать, пробовать ли себя в этом направлении или нет. ''')
+    if ((m[idt] - n[idt])/m[idt])*100 > 20:
+        _____('''У вас весьма большой доход. Надеюсь вы сможете продолжать в том же духе и исполните свои мечты!
 @celery_app.task
 def send_message():
     now = datetime.now()
@@ -114,8 +119,6 @@ def send_message():
     if now.day == 22 and now.hour == 10:
         message = "Hello, it's time to send the monthly report!"
         bot.send_message(CHAT_ID, message)
-
-
 if __name__ == '__main__':
     # Запускаем Celery
     celery_app.start()
